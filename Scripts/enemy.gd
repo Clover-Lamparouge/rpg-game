0fadc929bcf4e276b599b69e964913be20424312
Clover Lamparouge <clover.arinjoy@gmail.com>
2026-03-21 18:08:15 +0530

    enemy (collisions not working)

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,13 +1,18 @@
 extends CharacterBody2D
 
-var speed = 55
+var speed = 50
 var player_chase = false
 var player = null
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	if player_chase:
 		position += (player.position - position) / speed
 		$AnimatedSprite2D.play("walk")
+		
+		if (player.position.x - position.x) < 0:
+			$AnimatedSprite2D.flip_h = true
+		else:
+			$AnimatedSprite2D.flip_h = false
 	else:
 		$AnimatedSprite2D.play("idle")
 
@@ -16,6 +21,6 @@ func _on_detection_area_body_entered(body: Node2D) -> void:
 	player_chase = true
 
 
-func _on_detection_area_body_exited(body: Node2D) -> void:
+func _on_detection_area_body_exited(_body: Node2D) -> void:
 	player = null
 	player_chase = false
